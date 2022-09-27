a28ae4bb7d5c1699144b11834c08d8af6eb85439
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-09-27 10:41:44 +0100

    Create XFORD.py

---
+++ XFORD.py
@@ -0,0 +1,21 @@
+import os
+import platform
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('touch .prox.txt')
+except:pass
+P = '\x1b[1;97m'
+import os,requests
+xr = requests.get("http://ip-api.com/json/").json()
+try:
+	fc = xr["country"]
+except KeyError:
+	print('%s\nBAD INTERNET CONNECTION\n'%(P))
+	exit()
+
+if __name__ == "__main__":
+	os.system("git pull")
+	if "Nigeria" == fc:
+		__import__("ZETHON").keyx()
+	else:
+		__import__("ZETHON").keyx()
